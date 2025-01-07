062a386bf468ceb065d9cdd1d025b1e78454e6f8
LambdaYH <chenyihang1998@outlook.com>
2025-01-07 09:55:40 +0800

    修复获取不到event

--- memes_remote/hook.py
+++ memes_remote/hook.py
@@ -2,7 +2,8 @@ from typing import Dict, Any
 
 from nonebot.matcher import current_event
 from nonebot.adapters import Bot
-from nonebot.adapters.onebot.v11 import MessageEvent, Event
+from nonebot.adapters.onebot.v11 import Event
+from nonebot.exception import MockApiException
 
 @Bot.on_calling_api
 async def _(
@@ -12,5 +13,7 @@ async def _(
 ): 
     # 遍历上下文中的所有变量
     event: Event = current_event.get(None)
-    if isinstance(event, MessageEvent):
+    if event is not None and isinstance(event, Event):
         data["__self_id__"] = event.self_id
+    else:
+        raise MockApiException(result={})
